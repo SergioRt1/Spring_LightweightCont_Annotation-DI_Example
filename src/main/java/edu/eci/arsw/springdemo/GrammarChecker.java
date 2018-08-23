1be0c448da44815c2f32a854d6307a4078a360fa
unknown <2135494@labinfo.is.escuelaing.edu.co>
2018-08-23 17:23:07 -0500

    finish

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -1,20 +1,21 @@
 package edu.eci.arsw.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
+@Component
 public class GrammarChecker {
 
 	SpellChecker sc;
 
 	String x;
         
-        
 	public SpellChecker getSpellChecker() {
 		return sc;
 	}
-
+        
+        @Autowired
 	public void setSpellChecker(SpellChecker sc) {
 		this.sc = sc;
 	}
